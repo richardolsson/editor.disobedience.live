1b4f5ebde84deee9906f3bb1caa91ddfc3f7b00f
Richard Olsson <r@richardolsson.se>
2017-08-09 16:05:58 +0200

    Add format for Facebook headers

--- app/app.py
+++ app/app.py
@@ -1,10 +1,15 @@
 import os
 import tempfile
 import pdfkit
+import imgkit
 
 from flask import Flask, render_template, request, Response
 
 app = Flask(__name__)
+
+SIZES = {
+    'fbheader': (1200, 600),
+}
 
 @app.route('/')
 def index():
@@ -37,21 +42,35 @@ def asset():
         data['style'] = data['style'].replace('/static', static_path)
 
         html = render_template('asset.html', **data)
-        pdf = pdfkit.from_string(html, False, options={
-            'disable-smart-shrinking': '',
-            'page-width': '210',
-            'page-height': '297',
-            'margin-bottom': '0',
-            'margin-left': '0',
-            'margin-right': '0',
-            'margin-top': '0',
-        })
 
-        resp = Response(pdf)
-        resp.headers['Content-Type'] = 'application/pdf'
-        resp.headers['Content-Disposition'] = 'attachment; filename=asset.pdf'
+        if data['medium'] == 'print':
+            pdf = pdfkit.from_string(html, False, options={
+                'disable-smart-shrinking': '',
+                'page-width': '210',
+                'page-height': '297',
+                'margin-bottom': '0',
+                'margin-left': '0',
+                'margin-right': '0',
+                'margin-top': '0',
+            })
 
-        return resp
+            resp = Response(pdf)
+            resp.headers['Content-Type'] = 'application/pdf'
+            resp.headers['Content-Disposition'] = 'attachment; filename=asset.pdf'
+            return resp
+        elif data['medium'] == 'screen':
+            w, h = SIZES[data['format']]
+            img = imgkit.from_string(html, False, options={
+                'disable-smart-width': '',
+                'width': w,
+                'height': h,
+            })
+
+            resp = Response(img)
+            resp.headers['Content-Type'] = 'image/jpeg'
+            resp.headers['Content-Disposition'] = 'attachment; filename=asset.jpg'
+            return resp
+
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8000)
